507e23213c9a59f88eb9004bd1c29c2d4158a16b
Sayali Kumbhar <63386141+sayali0512@users.noreply.github.com>
2020-08-17 13:27:41 +0530

    updated code for install pre-requisites

--- scripts/setup_webserver.sh
+++ scripts/setup_webserver.sh
@@ -59,8 +59,8 @@ check_fileServerType_param $fileServerType
   # install pre-requisites
   #sudo apt-get -y install python-software-properties unzip rsyslog
   sudo apt-get install -y software-properties-common
-  sudo apt-get install unzip
-  sudo apt-get install rsyslog
+  sudo apt-get -y install unzip
+  sudo apt-get -y install rsyslog
   sudo apt-get -y install postgresql-client mysql-client git
 
   if [ $fileServerType = "gluster" ]; then
